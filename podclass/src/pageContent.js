2f9644a4c469eb01f05def78fec47ee1a9ffc7fd
Becks Maybury <becksmaybury@hotmail.co.uk>
2021-07-03 14:23:45 +0100

    initial content arrays added, button component working

---
+++ podclass/src/pageContent.js
@@ -0,0 +1,22 @@
+
+export const contentData = 
+    [
+    {id: 1, 
+    header:"Getting started" , 
+    subheadings:["Category and Topic", "Format", "Name"] , 
+    textContent: ["this is about category and topic", "this is about format", "this is about name"], 
+    images:["getting started 1", "getting started2"] 
+    },
+    {id: 2,
+    header:"Content" , 
+    subheadings:["Intro", "Main Content", "Outro"] , 
+    textContent: ["this is intro","this is main", "this is outro"], 
+    images:[["content 1"]] 
+    },
+]
+
+export const buttonTitles = [
+    "Getting Started",
+    "Content",
+    "Equipment"
+]
