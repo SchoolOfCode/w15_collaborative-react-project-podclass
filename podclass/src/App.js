b36bd5e34ff764373e94c786ada9154c72e0dc38
VicRenRen <kiiroinihil@gmail.com>
2021-07-05 21:14:44 +0100

    experimentations with CSS

--- podclass/src/App.js
+++ podclass/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    //Vic+ DELETE BELOW before merge
+    <div className="testingBoxDeleteLater">
+      <button className="nav-button">It is I hahaha! El button.</button>
+      <br></br>
+      <button className="nav-button">ON AIR</button>
+      <br></br>
+      <button className="nav-button">plz fear me!</button>
     </div>
+
+    //Vic+ DELETE ABOVE before merge
   );
 }
 
