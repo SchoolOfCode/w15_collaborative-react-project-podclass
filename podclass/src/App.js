59ff7ae3c7e4b3bacacd55230bc83c0775cf8289
Lewis Murray <lewisdmurray00@gmail.com>
2021-07-06 21:59:05 +0100

    Added some css and neon backgrounds, buttons spaced out

--- podclass/src/App.js
+++ podclass/src/App.js
@@ -43,7 +43,7 @@ const [contentState, setContentState] = useState("");
       {titles.map(
         (button) =>
           (button = (
-            <Button
+            <Button className="contentButtons"
               key={button}
               buttonName={button}
               identity={changeState}
