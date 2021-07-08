3cd8308177fff394c9dc869cc3b1e5afe1d49f00
Becks Maybury <becksmaybury@hotmail.co.uk>
2021-07-08 21:47:31 +0100

    ternary statement added to API function to allow for failed fetch request

--- podclass/src/components/API/index.js
+++ podclass/src/components/API/index.js
@@ -15,7 +15,7 @@ function Api({ search }) {
           method: "GET",
           headers: {
             "x-rapidapi-key":
-              "c6b7faf605msha0634e3b1f07049p1d0dbdjsn61e10d597152",
+              "855aee439dmshaa3857cc3a43bdap1d39e3jsn5f8f4aa03352",
             "x-rapidapi-host": "amazon23.p.rapidapi.com",
           },
         }
@@ -29,28 +29,19 @@ function Api({ search }) {
     getEquipment(search);
 }, [search]);
 
-return "bollocks to this"
-}
-
+return (
+    (equipmentSearch.length > 0 ? (
+    <div>
+    <p>{equipmentSearch.result[0].title}</p>
+    <p>{equipmentSearch.result[0].url} </p>
+    <p>{equipmentSearch.result[0].price.current_price}</p>
+    <img>{equipmentSearch.result[0].thumbnail}</img>
+    </div> ) :
+    (
+    <div> loading </div>
+    )   
+    )
+    )
+    }
 
 export default Api;
-
-// function PokemonViewer({ id }) {
-//     // TODO: send http request to `https://pokeapi.co/api/v2/pokemon/${id}` and display the data!
-//   const [pokemon, setPokemon] = useState({});
-
-//   useEffect(() => {
-//     async function getPokemon (id) {
-//       let response = await fetch (`https://pokeapi.co/api/v2/pokemon/${id}`);
-//       let data = await response.json();
-//       setPokemon (data)} getPokemon(id)}, [id]);
-//     // HINT: you will need useState and useEffect!
-//     return (
-//       <div className="pokemon-viewer">
-//         <p>display pokemon with id {id} here!</p>
-//         <p>{pokemon.name}</p>
-//       </div>
-//     );
-//   }
-
-//   export default PokemonViewer;
