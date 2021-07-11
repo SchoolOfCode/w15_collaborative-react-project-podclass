e7354c333ac43ceab77e1940554fe59dfae93512
VicRenRen <kiiroinihil@gmail.com>
2021-07-11 01:00:29 +0100

    further cleaning, added style for logo, checklist-item-content, and started sorting app.css

--- podclass/src/App.js
+++ podclass/src/App.js
@@ -13,37 +13,62 @@ import Logo from "../src/Images/logoButton.png";
 // import Api from "../src/components/API";
 
 function App() {
-    const [isOpen, setIsOpen] = React.useState(false);
-    const [content, setContent] = useState(contentData);
-    const [contentState, setContentState] = useState("0");
-    const [buttonClass, setButtonClass] = useState("nav-button");
-    const [completedChecklist, setCompletedChecklist] = useState([]);
-  
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [content, setContent] = useState(contentData);
+  const [contentState, setContentState] = useState("0");
+  const [buttonClass, setButtonClass] = useState("nav-button");
+  const [completedChecklist, setCompletedChecklist] = useState([]);
+
   // This is the array of checklist items that have been selected by the user
-    const selectedItems = content.reduce((acc, item) => {
-      item.sections.forEach((section) => {
-        section.checklist.forEach((checklistItem) => {
-          if (checklistItem.selected) {          
-            acc.push(checklistItem);} });
-          });
-      return acc;}, []);
+  const selectedItems = content.reduce((acc, item) => {
+    item.sections.forEach((section) => {
+      section.checklist.forEach((checklistItem) => {
+        if (checklistItem.selected) {
+          acc.push(checklistItem);
+        }
+      });
+    });
+    return acc;
+  }, []);
 
   // function to handle adding items for podlist display
-function handleChecklistItemClick(topicId, sectionId, checklistId) {
-      setContent((content) => {
-        const topicIndex = content.findIndex((topic) => topic.id === topicId);
-        const topicToUpdate = content[topicIndex];
-        const sectionIndex = topicToUpdate.sections.findIndex((section) => section.key === sectionId);
-        const sectionToUpdate = topicToUpdate.sections[sectionIndex];
-        const checklistIndex = sectionToUpdate.checklist.findIndex((checklistItem) => checklistItem.key === checklistId);
-        const itemToUpdate = sectionToUpdate.checklist[checklistIndex];
-        const updatedChecklistItem = {...itemToUpdate,selected: !itemToUpdate.selected,};
-        const updatedChecklist = [...sectionToUpdate.checklist.slice(0, checklistIndex),updatedChecklistItem,...sectionToUpdate.checklist.slice(checklistIndex + 1),];
-        const updatedSection = {...sectionToUpdate,checklist: updatedChecklist,};
-        const updatedSections = [...topicToUpdate.sections.slice(0, sectionIndex),updatedSection,...topicToUpdate.sections.slice(sectionIndex + 1),];
-        const updatedTopic = { ...topicToUpdate, sections: updatedSections };
-        const checkUpdate = [...content.slice(0, topicIndex),updatedTopic,...content.slice(topicIndex + 1),];
-        return checkUpdate;
+  function handleChecklistItemClick(topicId, sectionId, checklistId) {
+    setContent((content) => {
+      const topicIndex = content.findIndex((topic) => topic.id === topicId);
+      const topicToUpdate = content[topicIndex];
+      const sectionIndex = topicToUpdate.sections.findIndex(
+        (section) => section.key === sectionId
+      );
+      const sectionToUpdate = topicToUpdate.sections[sectionIndex];
+      const checklistIndex = sectionToUpdate.checklist.findIndex(
+        (checklistItem) => checklistItem.key === checklistId
+      );
+      const itemToUpdate = sectionToUpdate.checklist[checklistIndex];
+      const updatedChecklistItem = {
+        ...itemToUpdate,
+        selected: !itemToUpdate.selected,
+      };
+      const updatedChecklist = [
+        ...sectionToUpdate.checklist.slice(0, checklistIndex),
+        updatedChecklistItem,
+        ...sectionToUpdate.checklist.slice(checklistIndex + 1),
+      ];
+      const updatedSection = {
+        ...sectionToUpdate,
+        checklist: updatedChecklist,
+      };
+      const updatedSections = [
+        ...topicToUpdate.sections.slice(0, sectionIndex),
+        updatedSection,
+        ...topicToUpdate.sections.slice(sectionIndex + 1),
+      ];
+      const updatedTopic = { ...topicToUpdate, sections: updatedSections };
+      const checkUpdate = [
+        ...content.slice(0, topicIndex),
+        updatedTopic,
+        ...content.slice(topicIndex + 1),
+      ];
+      return checkUpdate;
     });
   }
 
@@ -56,52 +81,83 @@ function handleChecklistItemClick(topicId, sectionId, checklistId) {
   function changeButtonClass() {
     if (buttonClass === "nav-button") {
       setButtonClass("nav-button-clicked");
-        }
+    }
   }
 
-  function handleCompletedChecklist (i) {
-    setCompletedChecklist ([...selectedItems.slice(0,i), selectedItems[i].className="checklist-item-done",...selectedItems.slice(i+1)])
-     }
-   return (
- <>
+  function handleCompletedChecklist(i) {
+    setCompletedChecklist([
+      ...selectedItems.slice(0, i),
+      (selectedItems[i].className = "checklist-item-done"),
+      ...selectedItems.slice(i + 1),
+    ]);
+  }
+  return (
+    <>
       {/* <Api/> */}
 
-{/* Podlist/Checklist overlay */}          
- <div>
-  <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-     <h1>Your Podcast To-Do PodList</h1>
-         <ul>
-         {selectedItems.map((item, i) => (
-         <Checklist items={selectedItems[i]} key={selectedItems[i].key} className={selectedItems[i].className} handleCompleted = {handleCompletedChecklist} index={i}/>))}
-         </ul>
-  </Modal>
- </div>
+      {/* Podlist/Checklist overlay */}
+      <div>
+        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+          <h1>Your Podcast To-Do PodList</h1>
+          <ul>
+            {selectedItems.map((item, i) => (
+              <Checklist
+                items={selectedItems[i]}
+                key={selectedItems[i].key}
+                className={selectedItems[i].className}
+                handleCompleted={handleCompletedChecklist}
+                index={i}
+              />
+            ))}
+          </ul>
+        </Modal>
+      </div>
 
- {/* create page content buttons for nav bar  */}
- <div id={"navigation"}>
-   <div className="ButtonSeperator">
-     <img src={Logo} alt="logo" width="320" height="320" ></img>
-       {titles.map((button) => (button = (               
-        <Button key={button}
-          buttonName={button}
-          identity={changeState}
-          identityNumber={titles.indexOf(`${button}`)}
-          className={buttonClass}
-          onClick={changeButtonClass}/>)))
-        }
-   
- {/* create button to display checklist */}
-      <button className={buttonClass} onClick={() => {setIsOpen(true);}}>My PodList</button>
- </div>
- 
- {/* display content for each page */}
- <div className="content">
-          <Display addChecklistItem={handleChecklistItemClick}
-            content={content.filter((content) => content.id === contentState)}/>
-  </div>
- </div>
-</>
-);
+      {/* create page content buttons for nav bar  */}
+      <div id={"navigation"}>
+        <div className="ButtonSeperator">
+          <img
+            className="logo"
+            src={Logo}
+            alt="logo"
+            width="320"
+            height="320"
+          ></img>
+          {titles.map(
+            (button) =>
+              (button = (
+                <Button
+                  key={button}
+                  buttonName={button}
+                  identity={changeState}
+                  identityNumber={titles.indexOf(`${button}`)}
+                  className={buttonClass}
+                  onClick={changeButtonClass}
+                />
+              ))
+          )}
+
+          {/* create button to display checklist */}
+          <button
+            className={buttonClass}
+            onClick={() => {
+              setIsOpen(true);
+            }}
+          >
+            My PodList
+          </button>
+        </div>
+
+        {/* display content for each page */}
+        <div className="content">
+          <Display
+            addChecklistItem={handleChecklistItemClick}
+            content={content.filter((content) => content.id === contentState)}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- podclass/src/Modal/Modal.js
+++ podclass/src/Modal/Modal.js
@@ -1,48 +1,45 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
+import React from "react";
+import ReactDom from "react-dom";
 
 const modalStyles = {
-    position: 'fixed',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: '4fff',
-    padding: '50px',
-    zIndex: 1000
-}
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  backgroundColor: "4fff",
+  padding: "50px",
+  zIndex: 1000,
+};
 
 const overlayStyle = {
-    position: 'fixed',
-    top: 120,
-    left: 100,
-    right: 100,
-    bottom: 120,
-    backgroundColor: 'rgba(0,0,0,0.7)',
-    zIndex: 1000,
-    border: "4.5px solid #deffff",
-    boxShadow: "0 0 15px #ff5e00, 0 0 10px #ff4800, inset 0 0 15px #ff5e00",
-    // boxShadow: "0 0 20px #00ffff, 0 0 15px #00d9ff, inset 0 0 15px #00ffff",
-    borderRadius: "8px",
-    backdropFilter: "blur(10px)"
-}
+  position: "fixed",
+  top: 120,
+  left: 100,
+  right: 100,
+  bottom: 120,
+  backgroundColor: "rgba(0,0,0,0.7)",
+  zIndex: 1000,
+  border: "4.5px solid #deffff",
+  boxShadow: "0 0 15px #ff5e00, 0 0 10px #ff4800, inset 0 0 15px #ff5e00",
+  // boxShadow: "0 0 20px #00ffff, 0 0 15px #00d9ff, inset 0 0 15px #00ffff",
+  borderRadius: "8px",
+  backdropFilter: "blur(10px)",
+};
 
+export default function Modal({ open, children, onClose, selectedItems }) {
+  if (!open) return null;
 
-export default function Modal({open, children, onClose, selectedItems}){
-     
-  if (!open) return null
-
-    return ReactDom.createPortal(
-        <>
-        <div style={overlayStyle}/>
-        <div style={modalStyles}>
-        <button className="close-button" onClick={onClose}>Close</button>
-            {children}
+  return ReactDom.createPortal(
+    <>
+      <div style={overlayStyle} />
+      <div style={modalStyles}>
+        <button className="close-button" onClick={onClose}>
+          X
+        </button>
+        {children}
         <p>some text here</p>
-
-        </div>
-        </>,
-        document.getElementById('portal')
-    )
- }
- 
+      </div>
+    </>,
+    document.getElementById("portal")
+  );
+}
