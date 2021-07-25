7da4ece047bf4c78d7ce8c3eb3fa3bed70739f16
Catherine Butler <catherinebutler@Catherines-MacBook-Air.local>
2021-07-25 10:31:10 +0100

    written mock test for a click fn on button component

--- podclass/src/components/Button/button.test.js
+++ podclass/src/components/Button/button.test.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import Button from './';
 
-// const testProps = {
+const testProps = {
+    handleClick = jest.fn(),
+ };
 
-// }
-
-test('Check if button renders component renders', 
+test('Check if button component renders', 
     ()=>{
     render(<Button/>)
     const buttonComponent = screen.getByTestId("button-component");
     expect(buttonComponent).toBeInTheDocument()
+})
+
+test('Button calls the handleClick function when clicked', 
+    ()=>{
+    const { getByText } = render (<Button{...testProps}/>);
+    const button = getByText('');
+    fireEvent.click(button);
+    expect(testProps.handleClick).toHaveBeenCalled();
 })
